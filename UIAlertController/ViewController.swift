13989eb8292eb6fc84246812331dd19689292960
youngju <asdadssad@naver.coim>
2018-04-19 11:05:25 +0900

    UIAlertController

--- UIAlertController/ViewController.swift
+++ UIAlertController/ViewController.swift
@@ -20,6 +20,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+        let myaAlertConroller = UIAlertController(title: "알림", message: "설정한 시간이 되었습니다.",preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인",style: UIAlertActionStyle.default,handler: {
+            (myaction: UIAlertAction)->Void in
+            self.view.backgroundColor = UIColor.red
+            })
+        
+        let cancelAction = UIAlertAction(title: "취소",style: .cancel,handler: {
+            (myaction: UIAlertAction)->Void in
+            self.view.backgroundColor = UIColor.white
+        })
+        
+        myaAlertConroller.addAction(okAction)
+        myaAlertConroller.addAction(cancelAction)
+        
+        present(myaAlertConroller, animated: true, completion: nil)
+    }
+    
 }
 
